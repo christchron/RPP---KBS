c0b4af114d6c4d79f23a01c32b2415142ec77597
Vincent Theophilus Ciputra <vincent_theophilusc@yahoo.com>
2016-12-13 02:44:54 +0700

    add courses.js

---
+++ courses.js
@@ -0,0 +1,128 @@
+module.exports = {};
+
+module.exports.questions = [{
+  about: "purpose",
+  question: "Apa tujuan Anda dalam melakukan travelling ?",
+  options: [
+          "business",
+          "travelling",
+          "honeymoon",
+          "shopping",
+          "surfing",
+          "studying"]
+}, {
+  about: "budget",
+  question: "Berapa budget yang Anda sediakan untuk perjalanan ini ?",
+  options: [
+          "tinggi",
+          "sedang",
+          "rendah"]
+}, {
+  about: "continent",
+  question: "Benua manakah yang ingin Anda kunjungi ?",
+  options: [
+          "east asia",
+          "southeast asia",
+          "north america",
+          "south america",
+          "east europe",
+          "west europe"]
+}, {
+  about: "popularity",
+  question: "Seberapa populer tujuan travel Anda yang Anda harapkan ?",
+  options: [
+          "sangat populer",
+          "cukup populer",
+          "tidak populer"]
+}, {
+  about: "season",
+  question: "Musim apakah yang Anda inginkan pada saat melakukan travelling ?",
+  options: [
+          "spring",
+          "summer",
+          "autumn",
+          "winter"]
+}];
+
+module.exports.data = [{
+  course: '',
+  faculty: '',
+  department: '',
+  day: '',
+  hours: '',
+  time: '',
+  sks: '',
+  lecturer_type: ''
+}, {
+  course: '',
+  faculty: '',
+  department: '',
+  day: '',
+  hours: '',
+  time: '',
+  sks: '',
+  lecturer_type: ''
+}, {
+  course: '',
+  faculty: '',
+  department: '',
+  day: '',
+  hours: '',
+  time: '',
+  sks: '',
+  lecturer_type: ''
+}, {
+  course: '',
+  faculty: '',
+  department: '',
+  day: '',
+  hours: '',
+  time: '',
+  sks: '',
+  lecturer_type: ''
+}, {
+  course: '',
+  faculty: '',
+  department: '',
+  day: '',
+  hours: '',
+  time: '',
+  sks: '',
+  lecturer_type: ''
+}, {
+  course: '',
+  faculty: '',
+  department: '',
+  day: '',
+  hours: '',
+  time: '',
+  sks: '',
+  lecturer_type: ''
+}, {
+  course: '',
+  faculty: '',
+  department: '',
+  day: '',
+  hours: '',
+  time: '',
+  sks: '',
+  lecturer_type: ''
+},{
+  course: '',
+  faculty: '',
+  department: '',
+  day: '',
+  hours: '',
+  time: '',
+  sks: '',
+  lecturer_type: ''
+}, {
+  course: '',
+  faculty: '',
+  department: '',
+  day: '',
+  hours: '',
+  time: '',
+  sks: '',
+  lecturer_type: ''
+}]
